e1cb66320fd4873237fbdeac8b1c7db617d69326
Hanjaemo <hjm2530@gmail.com>
2023-05-08 23:57:26 +0900

    commit

---
+++ src/main/java/jscode/jscodestudy/JscodeStudyApplication.java
@@ -0,0 +1,13 @@
+package jscode.jscodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodeStudyApplication.class, args);
+	}
+
+}
