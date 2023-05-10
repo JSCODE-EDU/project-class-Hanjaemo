df942eebb37ff0a812cdfcee2518bd73bf93f7c5
Hanjaemo <hjm2530@gmail.com>
2023-05-10 21:30:35 +0900

    feat: create Post entity

--- src/main/java/jscode/jscodestudy/Post.java
+++ src/main/java/jscode/jscodestudy/Post.java
@@ -1,13 +1,17 @@
 package jscode.jscodestudy;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class TestEntity {
+public class Post {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
+    @Column(name = "post_id")
     private Long id;
+
+    private String title;
+    private String content;
 }
