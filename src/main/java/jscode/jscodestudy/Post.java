28ee7dd178329b59bb2044aeb62b629f9ffd1e1c
Hanjaemo <hjm2530@gmail.com>
2023-05-11 14:12:54 +0900

    feat: Post 엔티티 작성

--- src/main/java/jscode/jscodestudy/Post.java
+++ src/main/java/jscode/jscodestudy/Post.java
@@ -1,17 +1,18 @@
 package jscode.jscodestudy;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Post {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
 
     private String title;
     private String content;
+
+    @Column(name = "created_time", updatable = false)
+    private LocalDateTime createdTime = LocalDateTime.now();
 }
