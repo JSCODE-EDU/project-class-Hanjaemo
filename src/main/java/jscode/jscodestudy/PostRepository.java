82126cd806970d866433bd9dc2f87f9aaa3cbc9d
Hanjaemo <hjm2530@gmail.com>
2023-05-11 21:15:41 +0900

    feat: PostRepository 작성

---
+++ src/main/java/jscode/jscodestudy/PostRepository.java
@@ -0,0 +1,27 @@
+package jscode.jscodestudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+
+    private final EntityManager em;
+
+    public void save(Post post) {
+        em.persist(post);
+    }
+
+    public List<Post> findAll() {
+        return em.createQuery("select p from Post p", Post.class)
+                .getResultList();
+    }
+
+    public Post findOne(Long id) {
+        return em.find(Post.class, id);
+    }
+}
