0a6c7256294fd14103ea34a547e5827b9f48cea6
Hanjaemo <hjm2530@gmail.com>
2023-05-24 14:51:20 +0900

    no message

--- src/main/java/jscode/jscodestudy/dto/PostDto.java
+++ src/main/java/jscode/jscodestudy/dto/PostDto.java
@@ -1,5 +1,7 @@
 package jscode.jscodestudy.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jscode.jscodestudy.domain.Post;
 import lombok.Data;
 
@@ -9,17 +11,23 @@ import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
+@ApiModel(value = "게시글 정보")
 public class PostDto {
+
+    @ApiModelProperty(name = "id", value = "아이디", example = "1", required = true, dataType = "Long")
     private Long id;
 
     @NotBlank(message = "제목은 공백 없이 1글자 이상 15글자 이하여야 합니다.")
     @Size(min = 1, max = 15, message = "제목은 공백 없이 1글자 이상 15글자 이하여야 합니다.")
+    @ApiModelProperty(name = "title", value = "제목", example = "제목", required = true, dataType = "String")
     private String title;
 
     @NotEmpty(message = "내용은 1글자 이상 1000글자 이하여야 합니다.")
     @Size(min = 1, max = 1000, message = "내용은 1글자 이상 1000글자 이하여야 합니다.")
+    @ApiModelProperty(name = "content", value = "내용", example = "내용", required = true, dataType = "String")
     private String content;
 
+    @ApiModelProperty(name = "createdTime", value = "생성일자", required = true, dataType = "LocalDateTime")
     private LocalDateTime createdTime;
 
     public PostDto() {
